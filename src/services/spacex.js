85ec986192f61d23c475e4eba9bff4407bffa8d9
AgusFleitas <ezequiel-agustin-.-@hotmail.com>
2024-06-04 13:02:17 +0200

    First commit

---
+++ src/services/spacex.js
@@ -0,0 +1,34 @@
+export const getLatestLaunches = async () => {
+  const res = await fetch("https://api.spacexdata.com/v5/launches/query", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: {},
+      options: {
+        sort: {
+          date_unix: "asc",
+        },
+        limit: 12,
+      },
+    }),
+  });
+
+  const { docs } = await res.json();
+
+ docs.forEach(launch => {
+    console.log(launch.id);
+ });
+
+  return docs;
+};
+
+export const getLaunchById = async ({id}) => {
+  console.log('getLaunchById está recibiendo el ID: ' + id);
+  const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`);
+
+  const launch = await res.json();
+
+  return launch;
+};
